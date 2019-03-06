691404fb8cf9b1439bea9ad7da958cbac42decc6
Leonardo Dutra <leo.dutra012@gmail.com>
2019-03-06 12:56:45 -0300

    Added argparser for parsing credentials file

--- musicinfo.py
+++ musicinfo.py
@@ -1,4 +1,17 @@
+import argparse
+
 from app import App
+
+parser = parser = argparse.ArgumentParser(description = "App for information gathering from \
+														 Spotify and Lyrics Genius API's")		#defines argument parser with its description
+
+parser.add_argument('-f', '--f', type = str,
+					required = False, help = 'File with credentials')							#defines composer argument
+
+
+args = parser.parse_args()																		#gets parsed arguments
+
+
 
 init_message = '''
 \t***********************************
